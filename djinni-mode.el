ed07274da3cbaa012db3659471fa263d331d4cd0
Jason Foreman <jason@threeve.org>
2016-10-21 00:18:44 -0500

    Add basic indentation support.

--- djinni-mode.el
+++ djinni-mode.el
@@ -10,6 +10,17 @@
 
 ;;; Code:
 
+(defgroup djinni nil
+  "Major mode for editing Djinni IDL files."
+  :tag "Djinni"
+  :group 'languages)
+
+(defcustom djinni-indent-level 4
+  "Number of spaces per indentation level."
+  :type 'integer
+  :group 'djinni
+  :safe 'integerp)
+
 (defconst djinni-mode:syntax-table
   (let ((table (make-syntax-table)))
 
@@ -38,6 +49,18 @@
     ("\\<static\\>" . font-lock-keyword-face)
     ))
 
+(defun djinni-mode:indent-line ()
+  "Indent the current line."
+  (interactive)
+  (let ((indent-level))
+    (save-excursion
+      ;; find the current delimiter nesting level
+      (setq indent-level (nth 0 (syntax-ppss (point-at-bol))))
+      ;; reduce level by 1 if we're at the closing delimiter
+      (when (looking-at "\\s-*\\s)")
+        (setq indent-level (1- indent-level))))
+    (indent-line-to (* indent-level djinni-indent-level))))
+
 ;;;###autoload
 (define-derived-mode djinni-mode prog-mode "Djinni"
   :syntax-table djinni-mode:syntax-table
@@ -49,6 +72,10 @@
   (setq-local comment-padding " ")
   (setq-local comment-use-syntax t)
   (setq-local comment-auto-fill-only-comments t)
+
+  ;; indentation
+  (setq-local indent-line-function #'djinni-mode:indent-line)
+  (setq indent-tabs-mode nil)
 
   (setq font-lock-defaults '(djinni-mode:font-lock-defaults)))
 
